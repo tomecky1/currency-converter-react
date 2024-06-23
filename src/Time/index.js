d05c6732e65f3c0e168374d62eb8017cdcae6f5c
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-06-23 20:52:05 +0200

    minor fix in useState Date

--- src/Time/index.js
+++ src/Time/index.js
@@ -3,26 +3,34 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function Time() {
-    const [time, setTime] = useState(new Date().toLocaleString("pl-PL"));
-    console.log(time);
+  const [time, setTime] = useState(new Date().toLocaleString("pl-PL"));
+  console.log(time);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setTime(new Date().toLocaleString("pl-PL", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }));
-        }, 1000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(
+        new Date().toLocaleString("pl-PL", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "2-digit",
+        })
+      );
+    }, 1000);
 
-        return () => {
-            clearInterval(intervalId);
-        }
-    }
-        , []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <>
-            <p className="date">Dzisiaj jest {time}</p>
-        </>
-    );
-
-};
+  return (
+    <>
+      <p className="date">Dzisiaj jest {time}</p>
+    </>
+  );
+}
 
 export default Time;
