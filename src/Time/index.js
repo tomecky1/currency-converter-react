de3c350182a7cba850907616ed6404bfc568c5d9
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-06-29 15:09:29 +0200

    trying to impement custom hook useCurrentDate

--- src/Time/index.js
+++ src/Time/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = styled.time`
   font-size: small;
@@ -9,32 +9,11 @@ const Clock = styled.time`
 `;
 
 function Time() {
-  const [time, setTime] = useState(" ");
-  console.log(time);
-
-  const formattedDate = new Date().toLocaleString("pl-PL", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  });
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(formattedDate);
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [formattedDate]);
+  const date = useCurrentDate();
 
   return (
     <>
-      <Clock>Dzisiaj jest {time}</Clock>
+      <Clock>Dzisiaj jest {date}</Clock>
     </>
   );
 }
---
+++ src/Time/useCurrentDate.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const useCurrentDate = (date) => {
+  const [time, setTime] = useState(" ");
+  console.log(time);
+  const formattedDate = new Date().toLocaleString("pl-PL", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(formattedDate);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [formattedDate]);
+  <>return {setTime};</>;
+};
