2a25ca7ea06e3930530a699b10ed10b130809b22
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-07-30 15:52:31 +0200

    lates fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import Form from "./Form";
 import Container from "./Container";
-import styled from "styled-components";
 import axios from "axios";
 import { useWindowDimensions } from "./useWindowDimensions";
-
-const Title = styled.h1`
-  text-shadow: 1px 2px 5px yellow;
-  text-align: center;
-`;
-
-const Wrapper = styled.div`
-  padding: 10px;
-  background-color: #ccc;
-  text-align: center;
-  text-decoration: none;
-  border-radius: 5px;
-  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
-`;
+import { Title, Wrapper } from "./styled";
 
 function App() {
   axios
@@ -32,9 +18,6 @@ function App() {
         <Title>KANTOR</Title>
         <Form />
         <Wrapper>
-          <p className="kwota">Wartość waluty wynosi:</p>
-          <strong className="newCurrency js-result"> B/D</strong>
-          <br />
           Szerokość: {dimensions.width} Wysokość: {dimensions.height}
         </Wrapper>
       </Container>
---
+++ src/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  text-shadow: 1px 2px 5px yellow;
+  text-align: center;
+`;
+
+export const Wrapper = styled.div`
+  padding: 10px;
+  background-color: #ccc;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 5px;
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+`;
