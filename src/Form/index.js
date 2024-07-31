e55b99de7460e082e6f30a97fafce00260dd09c0
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-07-31 13:47:04 +0200

    setCurrency

--- src/Form/index.js
+++ src/Form/index.js
@@ -41,6 +41,7 @@ function Form() {
   const onFormReset = (event) => {
     event.preventDefault();
     setAmount(0); // reset input value
+    setCurrency("ADA");
   };
 
   const getExchangeDate = (ratesData) => {
